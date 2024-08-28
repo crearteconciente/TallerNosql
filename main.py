49a9e63d870fd1679c9e34ff95c68c6d49bbf532
“Andres <crearteconciente@gmail.com>
2024-08-27 21:05:04 -0500

    cambios

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import csv
 
 # Cargar los datos desde los archivos CSV
-with open('data/categorias.csv', mode='r', encoding='utf-8') as file:
+with open('categorias.csv', mode='r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     categorias = list(reader)
 
-with open('data/productos.csv', mode='r', encoding='utf-8') as file:
+with open('productos.csv', mode='r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     productos = list(reader)
 
 categorias_dict={} # se construye  un diccionario
-print("categorias") 
+#print("categorias") 
 for categoria in categorias[:5]: #se recorre categorias
-    print(categoria)
+    #print(categoria)
     categorias_dict[categoria['id']]={'nombre':categoria['modelo'],'productos':[]}
     
-print(categorias_dict)
+#print(categorias_dict)
 
-print('Productos')
+#print('Productos')
 productos_dict={}
 for producto in  productos:
     productos_dict[producto['id']]= {producto['marca'],producto['precio'],producto['unidades_disponibles']}
 
-union={[categorias_dict]:[productos_dict]}
-print(union)
+print(productos_dict)
